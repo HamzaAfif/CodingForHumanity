419c1429dc13acd8b268fc477b7ace45f0ef8034
Hamza AFIF <127676587+HamzaAfif@users.noreply.github.com>
2024-11-27 16:52:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = 10000;
 const db = new sqlite3.Database('./laBD.db');
 
 
